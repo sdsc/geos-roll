28efeac8027b360d5e0bd1aa7f0f50672fbb1934
Trevor Cooper <tcooper@sdsc.edu>
2013-11-20 16:54:19 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/geos/version.mk
+++ src/geos/version.mk
@@ -1,3 +1,14 @@
-NAME    = geos
-VERSION = 3.3.8
-RELEASE = 1
+NAME               = geos
+VERSION            = 3.3.8
+RELEASE            = 1
+PKGROOT            = /opt/geos
+
+SRC_SUBDIR         = geos
+
+SOURCE_NAME        = $(NAME)
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
